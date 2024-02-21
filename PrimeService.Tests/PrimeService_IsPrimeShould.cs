998cfa4ee3406b95dffb16395a5dfd5d9724bb16
zhenkaiwong1996 <zhenkaiwong.work@gmail.com>
2024-02-21 22:32:38 +0800

    feat: Add PrimeService and its test
    - Add PrimeService project
    - Add PrimeService type
    - Add PrimeService Unit Test project
    - Add PrimeService_IsPrimeShould test method
    - Add readme.md
    - Add dotnet gitignore file

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,24 @@
+using Xunit;
+
+namespace PrimeService.Tests;
+
+public class PrimeService_IsPrimeShould
+{
+    private readonly PrimeService _primeService;
+
+    public PrimeService_IsPrimeShould()
+    {
+        _primeService = new PrimeService();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void IsPrime_ValueLessThan2_ReturnFalse(int value)
+    {
+        bool result = _primeService.IsPrime(value);
+
+        Assert.False(result, $"{value} should not be prime");
+    }
+}
---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeService;
+
+public class PrimeService
+{
+    public bool IsPrime(int candidate)
+    {
+        if (candidate < 2)
+        {
+            return false;
+        }
+        throw new NotImplementedException("Not fully implemented.");
+    }
+}
